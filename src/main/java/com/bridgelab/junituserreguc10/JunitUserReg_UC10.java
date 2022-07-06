35d7ad36b66549eb9df30410025137b6e8155d52
ashishmuley1101 <ashishmuley1101@gmail.com>
2022-07-06 22:00:53 +0530

    my msg

--- src/main/java/com/bridgelab/junituserreguc10/JunitUserReg_UC10.java
+++ src/main/java/com/bridgelab/junituserreguc10/JunitUserReg_UC10.java
@@ -8,29 +8,29 @@ public class JunitUserReg_UC10 {
 	
 	UserRegImplemtation userreg = new UserRegImplemtation();
 	
-//	@Test
-//	public void checkFirstName() {
-//		String testFName = "Ashish";
-//		assertEquals(true, userreg.validateFName(testFName));
-//	}
-//	
-//	@Test
-//	public void checkLName() {
-//		String testLName = "Muley";
-//		assertEquals(true, userreg.validateLname(testLName));
-//	}
+	@Test
+	public void checkFirstName() {
+		String testFName = "Ashish";
+		assertEquals(true, userreg.validateFName(testFName));
+	}
 	
-//	@Test
-//	public void checkEmail() {
-//		String testEmail = "ashishmuley1101@gmail.com";
-//		assertEquals(true, userreg.valdateEmail(testEmail));
-//	}
+	@Test
+	public void checkLName() {
+		String testLName = "Muley";
+		assertEquals(true, userreg.validateLname(testLName));
+	}
 	
-//	@Test
-//	public void checkphoneNumber() {
-//		String testPhoneNumber = "91 9579112523";
-//		assertEquals(true, userreg.validatephoneNumber(testPhoneNumber));
-//	}
+	@Test
+	public void checkEmail() {
+		String testEmail = "ashishmuley1101@gmail.com";
+		assertEquals(true, userreg.valdateEmail(testEmail));
+	}
+	
+	@Test
+	public void checkphoneNumber() {
+		String testPhoneNumber = "91 9579112523";
+		assertEquals(true, userreg.validatephoneNumber(testPhoneNumber));
+	}
 	
 	@Test
 	public void checkPwd() {
